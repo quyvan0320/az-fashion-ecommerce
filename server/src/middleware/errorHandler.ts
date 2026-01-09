3045f23a779d683fd598ba14cded16e20eee904e
quyvan0320 <quyvan0320@gmail.com>
2026-01-09 20:47:33 +0700

    feat: create auth register service

--- server/src/middleware/errorHandler.ts
+++ server/src/middleware/errorHandler.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 
+
+// Custom Error Class
 export class AppError extends Error {
   statusCode: number;
   isOperational: boolean;
@@ -11,7 +13,7 @@ export class AppError extends Error {
     Error.captureStackTrace(this, this.constructor);
   }
 }
-
+  // Global Error Handling Middleware
 export const errorHandler = (
   err: any,
   req: Request,
@@ -20,12 +22,13 @@ export const errorHandler = (
 ) => {
   let statusCode = err.statusCode || 500;
   let message = err.message || "Lỗi máy chủ nội bộ";
-
+  // prisma unique error 
   if (err.code === "P2002") {
     statusCode = 400;
     message = "Dữ liệu đã tồn tại.";
   }
 
+  // prisma not found error
   if (err.code === "P2025") {
     statusCode = 404;
     message = "Dữ liệu không tồn tại.";
---
+++ server/src/services/auth.services.ts
@@ -0,0 +1,53 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import prisma from "../config/prisma";
+import { AppError } from "../middleware/errorHandler";
+
+
+export const authService = {
+  // register new user
+  async register(data: {
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    phone?: string;
+  }) {
+    // checke if user already exists
+    const existingUser = await prisma.user.findUnique({
+      where: { email: data.email },
+    });
+
+    if (existingUser) {
+      throw new AppError("Nguời dùng đã tồn tại", 400);
+    }
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    // create new user in database
+    const user = await prisma.user.create({
+      data: {
+        ...data,
+        password: hashedPassword,
+      },
+      select: {
+        // get selected fields only
+        id: true,
+        email: true,
+        firstName: true,
+        lastName: true,
+        phone: true,
+        role: true,
+        createdAt: true,
+      },
+    });
+
+    const token = jwt.sign(
+      { userId: user.id, role: user.role },
+      process.env.JWT_SECRET || "secret",
+      { expiresIn: process.env.JWT_EXPIRES_IN || "7d" }
+    );
+    return { user, token };
+  },
+};
