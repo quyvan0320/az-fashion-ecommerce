e228ad99f318f5b0791b95b758edd42a66078f2e
quyvan0320 <quyvan0320@gmail.com>
2026-01-25 16:44:59 +0700

    feat: Create product service

--- server/src/services/product.service.ts
+++ server/src/services/product.service.ts
@@ -1,13 +1,62 @@
+import prisma from "../config/prisma";
+import { CreateProductInput } from "../interfaces/product.interface";
+import { AppError } from "../middleware/errorHandler";
+import { generateSKU, generateSlug } from "../utils/string.util";
 
+export const productService = {
+  //create product
+  async create(data: CreateProductInput) {
+    // check category exists
+    const categoryId = await prisma.category.findUnique({
+      where: { id: data.categoryId },
+    });
 
-interface CreateProductInput {
-  name: string;
-  description?: string;
-  price: number;
-  salePrice?: number;
-  sku?: string;
-  stock: number;
-  categoryId: string;
-  images: string[];
-  isActive?: boolean;
-}
+    if (!categoryId) {
+      throw new AppError("Danh mục không tồn tại", 404);
+    }
+
+    // generate slug
+    const slug = generateSlug(data.name);
+
+    // check slug unique
+    const existingSlug = await prisma.product.findUnique({ where: { slug } });
+    const finalSlug = existingSlug ? `${slug}-${Date.now()}` : slug;
+
+    // generate SKU if not provide
+    const sku = data.sku || generateSKU(data.name, data.categoryId);
+
+    // check unique SKU
+    const existingSKU = await prisma.product.findUnique({
+      where: { sku },
+    });
+    if (existingSKU) {
+      throw new AppError("Mã sản phẩm đã tồn tại", 400);
+    }
+
+    // validate sale price
+    if (data.salePrice && data.salePrice >= data.price) {
+      throw new AppError(
+        "Mức giảm giá phải thấp hơn mức giá thông thường",
+        400,
+      );
+    }
+
+    //create
+    return prisma.product.create({
+      data: {
+        ...data,
+        slug: finalSlug,
+        sku,
+      },
+      include: {
+        category: {
+          select: {
+            id: true,
+            name: true,
+            slug: true,
+          },
+        },
+      },
+    });
+  },
+};
---
+++ server/src/utils/string.util.ts
@@ -0,0 +1,19 @@
+export const generateSlug = (name: string): string => {
+  return name
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "") // remove diacritics;
+    .replace(/[đĐ/]/g, "d") // replace đ and Đ
+    .replace(/[^a-z0-9\s-]+/g, "") // replace non-alphanumeric characters with hyphens
+    .trim()
+    .replace(/\s+/g, "-") // replace spaces with hyphens
+    .replace(/-+/g, "-"); // replace multiple hyphens with single hyphen
+};
+
+// generate unique SKU
+export const generateSKU = (name: string, categoryId: string): string => {
+  const prefix = name.substring(0, 3).toUpperCase();
+  const catePrefix = categoryId.substring(0, 3).toUpperCase();
+  const timestamp = Date.now().toString().slice(-6);
+  return `${prefix}-${catePrefix}-${timestamp}`;
+};
