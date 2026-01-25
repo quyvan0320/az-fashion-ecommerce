5817ef9b9990f45ffaa9e2c0e1bab32b52d59681
quyvan0320 <quyvan0320@gmail.com>
2026-01-25 18:08:04 +0700

    feat: Create product controller

---
+++ server/src/controllers/product.controller.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from "express";
+import { validationResult } from "express-validator";
+import { AppError } from "../middleware/errorHandler";
+import { productService } from "../services/product.service";
+
+export const productController = {
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        throw new AppError(
+          errors
+            .array()
+            .map((err) => err.msg)
+            .join(""),
+          400,
+        );
+      }
+
+      const product = await productService.create(req.body);
+      res
+        .status(201)
+        .json({
+          status: 200,
+          message: "Sản phẩm đã được tạo thành công",
+          data: product,
+        });
+    } catch (error) {
+      next(error);
+    }
+  },
+};
