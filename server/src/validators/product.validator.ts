a5fe1556f99cd563c8d7595cd2a22c3b9f686813
quyvan0320 <quyvan0320@gmail.com>
2026-01-25 17:27:22 +0700

    feat: Create product validator

---
+++ server/src/validators/product.validator.ts
@@ -0,0 +1,63 @@
+import { param, body, query } from "express-validator";
+
+export const createProductValidator = [
+  body("name")
+    .trim()
+    .notEmpty()
+    .withMessage("Tên không được để trống")
+    .isLength({ min: 3, max: 100 })
+    .withMessage("Tên phải dài từ 3 đến 100 ký tự"),
+
+  body("description")
+    .optional()
+    .trim()
+    .isLength({ max: 2000 })
+    .withMessage("Mô tả không vượt quá 2000 ký tự"),
+
+  body("price")
+    .notEmpty()
+    .withMessage("Giá không được để trống")
+    .isInt({ min: 0 })
+    .withMessage("Giá phải là số nguyên dương"),
+
+  body("salePrice")
+    .optional()
+    .isInt({ min: 0 })
+    .withMessage("Giá phải là số nguyên dương"),
+
+  body("sku")
+    .optional()
+    .trim()
+    .isLength({ min: 3, max: 50 })
+    .withMessage("SKU không vượt quá 2000 ký tự")
+    .matches(/^[A-Z0-9-]+$/)
+    .withMessage("SKU chỉ chưa ký tự in hoa, số và gạch nối"),
+
+  body("stock")
+    .notEmpty()
+    .withMessage("Hàng hóa không được để trống")
+    .isInt({ min: 0 })
+    .withMessage("Hàng hóa phải là số nguyên dương"),
+
+  body("categoryId")
+    .notEmpty()
+    .withMessage("Danh mục không được để trống")
+    .isUUID()
+    .withMessage("ID danh mục không hợp lệ"),
+
+  body("images")
+    .notEmpty()
+    .withMessage("Cần có ít nhất 1 hình ảnh")
+    .isArray({ min: 1, max: 10 })
+    .withMessage("Phải cung cấp 1 đến 10 hình ảnh"),
+
+  body("images.*")
+    .trim()
+    .isURL()
+    .withMessage("Mỗi hình ảnh phải là URL hợp lệ"),
+
+  body("isActive")
+    .optional()
+    .isBoolean()
+    .withMessage("isActive phải là kiểu Boolean"),
+];
