c7d5abcd62c178a92785e65c6db6ac5455c4c03b
quyvan0320 <quyvan0320@gmail.com>
2026-01-26 19:53:13 +0700

    feat: Get by slug product route

--- server/src/routes/product.route.ts
+++ server/src/routes/product.route.ts
@@ -4,6 +4,7 @@ import {
   createProductValidator,
   getProductsValidator,
   productIdValidator,
+  productSlugValidator,
 } from "../validators/product.validator";
 import { productController } from "../controllers/product.controller";
 
@@ -23,5 +24,9 @@ router.get("/", getProductsValidator, productController.getAll);
 
 // get by id
 router.get("/:id", productIdValidator, productController.getById);
+
+
+// get by slug
+router.get("/slug/:slug", productSlugValidator, productController.getBySlug);
 
 export default router;
