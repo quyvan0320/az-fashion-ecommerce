b7471472ad7f10f6c9f8b842be403218d06f6a3c
quyvan0320 <quyvan0320@gmail.com>
2026-02-18 20:22:00 +0700

    feat: Set up file structure and formatters ccurency date time

---
+++ client/src/utils/formatters.ts
@@ -0,0 +1,24 @@
+export const formatCurrency = (amount: number): string => {
+  return new Intl.NumberFormat("vi-VN", {
+    style: "currency",
+    currency: "VND",
+  }).format(amount);
+};
+
+export const formatDate = (date: string): string => {
+  return new Intl.DateTimeFormat("vi-VN", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }).format(new Date(date));
+};
+
+export const formatDateTime = (date: string): string => {
+  return new Intl.DateTimeFormat("vi-VN", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  }).format(new Date(date));
+};
