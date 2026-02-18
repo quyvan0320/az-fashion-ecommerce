78ca2a5f48833563f602616b13bde469d1136290
quyvan0320 <quyvan0320@gmail.com>
2026-02-18 20:50:58 +0700

    feat: Set up axios and react query

---
+++ client/src/services/api/axios.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { API_URL, STORAGE_KEYS } from "@/config/constants";
+
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+//request interceptor - add token
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem(STORAGE_KEYS.ACCESS_TOKEN);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+//request interceptor - handle errors
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      // unauthorized - clear token and redirect to login
+      localStorage.removeItem(STORAGE_KEYS.ACCESS_TOKEN);
+      localStorage.removeItem(STORAGE_KEYS.USER);
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default axiosInstance;
